aaddae587c9aa653e0c42c710928c1ada88a7b63
Flavian <Flavianmohanraj@gmail.com>
2020-01-14 19:02:58 +0000

    typewriting

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() =>{
+    process.stdout.write(char);
+  }, delay += 50)
+}
+
+let timer = sentence.length *50 
+setTimeout(() =>{
+  console.log()
+},timer)
